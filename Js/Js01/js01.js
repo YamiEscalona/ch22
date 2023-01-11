ecbd1a2a2923db85b54b485705e8ec9e7876c6ab
Yami Escalona <yamescalord@gmail.com>
2023-01-11 14:00:45 -0600

    ci: conversión de tipos de datos

--- Js/Js01/js01.js
+++ Js/Js01/js01.js
@@ -1,1 +1,23 @@
 console.log("Sesión js01");
+
+
+/*son 7 datos primitivos JavaSccript
+- String ("Hola Mundo")
+- Number (1800.22)
+    Los números se utilizan 64 bits para almacenarlo
+    pero, solo se usan 53 bits para representar el número.
+- BigInt (1800n)
+    Convertir un dato númerico a BigINT
+-Boolean
+-Undefined (undefined)
+    
+-Null
+-Symbol
+
+Tipos de datos Object:
+-Object
+-Array
+
+Funciones
+*/
+/*
