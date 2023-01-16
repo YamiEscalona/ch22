967ed74e745e800893746fbca09e45e9937a72b2
Yami Escalona <yamescalord@gmail.com>
2023-01-16 13:30:22 -0600

    ci: FIFO y LIFO

--- Js/Js04/js04.js
+++ Js/Js04/js04.js
@@ -81,3 +81,130 @@ for (let arma of armasCh22)
 //iterar de forma descendente las armas.
 for( let i = armasCh22.length -1; i>=0 ; i--) 
     console.log("Arma: " + armasCh22[i]);
+
+//Para el ciclo for no es necesario indicar en inicio, la comparación, la expresión final
+let iteracion = 0;
+for (; ; iteracion++) {
+    console.log("valor de i" + iteracion);
+    if (iteracion===10) break;
+    iteracion++;
+}
+//La instrucción break rompe con la interación.
+//La instrucción continue continua con la sig. iteración
+for (let i = 0; i <=10; i++) {
+    if(i===5)break;
+    console.log("For con break: "+i); // 0, 1, 2, 3, 4
+}
+
+for (let i = 0; i <=10; i++) {
+    if(i===5)continue;
+    console.log("For con continue: "+i); // 0, 1, 2, 3, 4, 6, 7, 8, 9, 10
+}
+    //[[],[],[]]
+const participantes = [["José","María","Pedro"], //Ch1
+["Tan","Javi","Andrea","Santi"], //Ch2
+["Melanie","Pavel",] //Ch3
+];
+                                //Feliz Cumpleaños
+                                //F   C
+console.log("Persona Ch3(f2) 2a persona(c1) "+participantes[2][1]);
+console.log("Persona Ch2(f1) 2a persona(c0) "+participantes[1][0]);
+console.log("Persona Ch1(f0) 2a persona(c2) "+participantes[0][2]);
+
+for(let fila=0 ; fila<participantes.length; fila++){
+    console.log(participantes[fila]); //Iterando las filas
+    for(let columna=0 ; columna< participantes[fila].length ; columna++){
+        console.log(participantes[fila][columna]);
+    }
+}
+//Usando break en ciclo anidados
+//Dejar de mostrar los nombre, cuando se encuentre a Tan
+//Usando una variable como bandera
+let continuarIteraciones = true;
+for(let fila=0 ; fila<participantes.length; fila++){
+        for(let columna=0 ; columna< participantes[fila].length ; columna++){
+            console.log(`[${fila}][${columna}]`);
+            if(continuarIteraciones)
+                console.log("Uso de break " + participantes[fila][columna]);
+            if(participantes[fila][columna] === "Tan") continuarIteraciones= false;
+        }
+}
+
+//Usando break
+cicloFila:
+for(let fila=0 ; fila<participantes.length; fila++){
+    cicloColumna:
+    for(let columna=0 ; columna< participantes[fila].length ; columna++){
+        console.log(`[${fila}][${columna}]`);
+         console.log("Uso de break " + participantes[fila][columna]);
+        if(participantes[fila][columna] === "Tan") break cicloFila;
+    }
+}
+
+//Usando continue
+cicloFila:
+for(let fila=0 ; fila<participantes.length; fila++){
+    cicloColumna:
+    for(let columna=0 ; columna< participantes[fila].length ; columna++){
+        if(participantes[fila][columna] === "Javi") continue cicloFila;
+        console.log(`[${fila}][${columna}]`);
+        console.log("Uso de continue " + participantes[fila][columna]);
+    }
+}
+
+//Ciclo while
+/*Sintaxis:
+while(condicion_verdadera) instrucción;
+while(condicion_verdadera) {
+    instrucción;}
+ */
+let numIteracion = 0;
+while (confirm("Quieres seguir iterando?")) {
+    console.log("Iterando n. "+ numIteracion++);
+}
+ // Preguntar del 1 al 10 y saber si es el número que pensó el usuario
+ let numero =1;
+while(!confirm(`${numero} es tu numero?`)) {
+    numero++;
+    if (numero === 10) break;
+}
+console.log("tu número es el " + numero);
+
+while(!confirm(`${numero++} es tu numero?`));
+console.log("tu número es el " + --numero);
+
+//ciclo do-while
+/*Sintasis:
+do{
+    instrucciones;
+} while(condicion_verdadera);
+*/
+let valor = 0;
+while(valor<5){console.log("While "+valor);
+valor++;
+}
+
+valor = 10;
+do{
+    console.log("do while "+ valor);
+    valor++;
+}while (valor < 5);
+
+/**
+ * FIFO: first input first output alimentos
+ * LIFO: last input first output herramienta */ 
+const alimentosPerecederos = []
+//Agregar una caja.
+// Usando FIFO
+alimentosPerecederos.push("manzanas lunes");
+alimentosPerecederos.push("peras miércoles");
+//Sacando manzanas del lunes
+console.log("sacando a la venta " + alimentosPerecederos.shift());//lunes
+console.log("sacando a la venta " + alimentosPerecederos.shift());//miércoles
+
+//Tenemos uan ferretería
+const productos = ["desarmadores","cemento"];
+//agrego luces navideñas de diciembre
+productos.push("luces navideñas");
+console.log("sacando a la venta " + productos.pop());
+console.log("sacando a la venta " + productos.pop());
